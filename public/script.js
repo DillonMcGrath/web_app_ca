385219be6ff1f3a6cae8e65ec434205fcbd0597b
Glitch (topic-02-lab-rb-25) <none>
2025-03-07 07:15:14 +0000

    🌛🏎 Checkpoint ./views/page2.hbs:41278841/154 ./public/style.css:41278841/702 ./public/script.js:41278841/278

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("popup").style.display = "block";
+    }, 1000); // 1 second delay
+});
 
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
