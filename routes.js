ac9b9cb1d289e7e17dcbc5d5684fd79dab3e80a2
Glitch (topic-02-lab-rb-25) <none>
2025-03-27 09:59:27 +0000

    👬🏅 Checkpoint ./routes.js:41278841/225 ./views/page5.hbs:41278841/3726 ./public/style.css:41278841/534

--- routes.js
+++ routes.js
@@ -62,12 +62,7 @@ router.get("/page5", (req, res) => {
 
 
 
-const carsData = JSON.parse(fs.readFileSync('./data/cars.json', 'utf8'));
 
-router.get("/page5", (req, res) => {
-  console.log("Cars Data:", carsData);  // Debugging halemary
-  res.render("page5", { cars: carsData.cars });
-});
 
 
 const mechanicsFile = './data/mechanics.json';
