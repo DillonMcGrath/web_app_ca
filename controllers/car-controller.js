d8be0d02a2488e6f7a00ad9870333d21f0dbaf5f
Glitch (topic-02-lab-rb-25) <none>
2025-04-08 18:37:17 +0000

    🌺🌛 Checkpoint ./controllers/car-controller.js:41278841/1436

--- controllers/car-controller.js
+++ controllers/car-controller.js
@@ -1,36 +1,9 @@
-// /controllers/car-controller.js
+// car-controller.js
+import { getAllCarBrands } from "../models/car-store.js";
 
-import fs from 'fs';
+function showCarBrands(req, res) {
+  const brands = getAllCarBrands();
+  res.render("page5", { brands });
+}
 
-// Load and parse the cars.json file
-const carsData = JSON.parse(fs.readFileSync('./data/cars.json', 'utf8'));
-
-// Show list of car brands
-const showCarBrands = (req, res) => {
-  res.render("page5", { brands: carsData.brands });
-};
-
-// Show models under a selected brand
-const showCarModels = (req, res) => {
-  const brand = req.params.brand;
-  const selectedBrand = carsData.brands.find(b => b.name.toLowerCase() === brand.toLowerCase());
-  if (!selectedBrand) return res.status(404).send("Brand not found");
-  res.render("page5-models", { brand: selectedBrand });
-};
-
-// Show details of a selected model
-const showCarDetails = (req, res) => {
-  const brand = req.params.brand;
-  const modelId = req.params.modelId;
-  const selectedBrand = carsData.brands.find(b => b.name.toLowerCase() === brand.toLowerCase());
-  if (!selectedBrand) return res.status(404).send("Brand not found");
-  const model = selectedBrand.models.find(m => m.id === modelId);
-  if (!model) return res.status(404).send("Model not found");
-  res.render("page5-details", { brand: selectedBrand.name, model });
-};
-
-export default {
-  showCarBrands,
-  showCarModels,
-  showCarDetails
-};
+export default { showCarBrands };
